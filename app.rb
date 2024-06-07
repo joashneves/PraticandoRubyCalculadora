dad0da08f341cd3d58a4328f6c8b28f8faa8d60a
joashneves <joashneves@gmail.com>
2024-06-07 09:52:39 -0300

    mudanças

--- app.rb
+++ app.rb
@@ -1,31 +1,46 @@
-print 'Fazer calcular (+, -, *, /): '
-calculo = gets.chomp
+print 'Fazer calcular: '
+calculo = gets.chomp  
+novoCalculo = calculo.split("")
 
-# Solicita a entrada do usuário e converte para inteiro imediatamente
-print 'Numero 1: '
-x = gets.to_i
-print 'Numero 2: '
-x2 = gets.to_i
-result = 0
-case calculo
-when "+"
-    print 'soma'
-# Realiza a soma dos valores inteiros
-result = x + x2
-when "-"
-# Realiza a soma dos valores inteiros
-result = x - x2
-when "*"
-result = x * x2
-when "/"
-    if x2 == 0
-        puts 'Não é possivel dividir por 0'
-    else 
-        result = x / x2
+puts "\n"
+print novoCalculo
+puts "\n"
+# Inicializa variáveis para armazenar os números
+x = nil
+x2 = nil
+
+# Inicializa o resultado
+result = nil
+
+# Itera sobre os caracteres da expressão
+novoCalculo.each_with_index do |char, index|
+    if ["+", "-", "*", "/"].include?(char)
+      # Converte as partes da expressão para inteiros
+      x = novoCalculo[0...index].join.to_i
+      x2 = novoCalculo[(index+1)..-1].join.to_i
+  
+      case char
+      when "+"
+        puts 'Operação: soma'
+        result = x + x2
+      when "-"
+        puts 'Operação: subtração'
+        result = x - x2
+      when "*"
+        puts 'Operação: multiplicação'
+        result = x * x2
+      when "/"
+        if x2 == 0
+          puts 'Não é possível dividir por 0'
+        else
+          puts 'Operação: divisão'
+          result = x / x2
+        end
+      else
+        puts 'Operação inválida'
+      end
     end
-else
-    puts 'Operação inválida'
-end
+  end
 # Imprime o resultado, se houver um
 if result
     puts "Resultado: #{x} #{calculo} #{x2} = #{result}"
